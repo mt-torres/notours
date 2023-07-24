327378b7ae9f30f00da0d83bf1bfeeb2c465d37e
mt-torres <mf_torres@yahoo.com.br>
2023-07-24 14:01:57 -0300

    Create middlaware and install slugify

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,5 @@
 const { mongoose } = require('mongoose');
-
+const slugify = require('slugify');
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -8,6 +8,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -70,7 +71,22 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
-// tourSchema.virtual('durationWeeks').get(() => this.duration / 7);
+// tourSchema.virtual('durationWeeks').get(() => this.duration / 7); //we need to use a normal function due to the 'this'
+// document middleware: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema); //creating the module
 
