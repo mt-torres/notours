6aba5513f17f0e94e0cbb8e3675f64500c78b31b
mt-torres <mf_torres@yahoo.com.br>
2023-08-15 21:34:51 -0300

    bug fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
